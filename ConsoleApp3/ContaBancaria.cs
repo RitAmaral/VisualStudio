ee1afa34e047ac9c387469e843697855a9ec20e4
RitAmaral <anaritamaral1995@gmail.com>
2023-05-05 09:33:17 +0100

    Criamos o exercício da conta bancária

---
+++ ConsoleApp3/ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class ContaBancaria
+    {
+        //~~Definição de propriedades:
+        public int NumConta { get; } //não tem set porque não se pode alterar o num de conta
+        public string Nome { get; set; }
+        public double Saldo { get; private set; } //para modificar(set) é privado
+
+        //~~Construtores:
+        public ContaBancaria(int numconta, string nome) //este construtor permite instanciar contas bancárias
+                                                      //apenas com o nº de conta e o seu titular
+        {
+            NumConta = numconta;
+            Nome = nome;
+        }
+        //Este 2ºconstrutor permite instanciar uma conta a partir do seu num de conta, nome e tb do seu saldo inicial:
+        public ContaBancaria(int numconta, string nome, double saldo):this(numconta, nome) //this = reutilizar código
+        {
+            Saldo = saldo;
+        }
+
+        //~~Secção de Métodos:
+
+        //Este é o método que permite depositar dinheiro na conta
+        public void Deposito(double quantia) 
+        { Saldo += quantia; }
+        //Este é o método que permite fazer o levantamento do dinheiro
+        public void Levantamento(double quantia) 
+        { Saldo -= quantia + 5.00; }
+
+
+        public override string ToString()
+        {
+            return "Conta Bancária número "
+                + NumConta
+                + ", nome do titular: " + Nome
+                + ", saldo: " + Saldo.ToString("F2") + " euros.";
+        }
+        
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApp3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Exercício do Banco - melhorado
+
+        ContaBancaria cb;
+
+        Console.WriteLine("Entre com o nº de conta: ");
+        int numconta = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nome do titular da conta: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Deseja fazer um depósito inicial? S/N");
+        char resp = char.Parse(Console.ReadLine());
+        if (resp == 'S' || resp == 's')
+        {
+            Console.WriteLine("Insira o valor do depósito inicial: ");
+            double depositoInicial = double.Parse(Console.ReadLine());
+            cb = new ContaBancaria(numconta, nome, depositoInicial);
+        }
+        else { cb = new ContaBancaria(numconta, nome); }
+
+        Console.WriteLine();
+        Console.WriteLine("Dados da conta: " + "\n" + cb);
+        Console.WriteLine();
+        Console.WriteLine("Valor a depositar: ");
+        double quantia = double.Parse(Console.ReadLine());
+        cb.Deposito(quantia);
+        Console.WriteLine("Dados atualizados da conta: ");
+        Console.WriteLine(cb);
+
+        Console.WriteLine();
+        Console.WriteLine("Que valor pretende levantar? ");
+        quantia = double.Parse(Console.ReadLine());
+        cb.Levantamento(quantia);
+        Console.WriteLine("Dados atualizados da conta: ");
+        Console.WriteLine(cb);
+
+        //Se quiser mudar o nome, ou seja, utilizar o set:       
+        cb.Nome = "Pedro";
+        Console.WriteLine(cb);
+    }
+}
